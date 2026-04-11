3f88331faa49935c5dad39245d3f5ff7f73d7eed
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2026-04-12 00:01:13 +0530

    Refactor LsCommand to avoid shell ls

--- sample/src/main/kotlin/com/srilakshmikanthanp/vshell/sample/commands/LsCommand.kt
+++ sample/src/main/kotlin/com/srilakshmikanthanp/vshell/sample/commands/LsCommand.kt
@@ -3,25 +3,29 @@ package com.srilakshmikanthanp.vshell.sample.commands
 import com.srilakshmikanthanp.vshell.jvm.command.Command
 import com.srilakshmikanthanp.vshell.jvm.command.CommandBuilder
 import com.srilakshmikanthanp.vshell.jvm.command.CommandBuilderDescriptor
+import com.srilakshmikanthanp.vshell.jvm.command.builtins.CommandException
+import com.srilakshmikanthanp.vshell.jvm.command.builtins.Input
+import com.srilakshmikanthanp.vshell.jvm.command.builtins.Output
+import com.srilakshmikanthanp.vshell.jvm.command.builtins.TextCapableCommand
 import com.srilakshmikanthanp.vshell.jvm.context.Context
-import java.io.InputStream
-import java.io.OutputStream
-import java.util.concurrent.Executors
 
-class LsCommand(private val context: Context, private val args: List<String>) : Command {
-  override fun execute(stdIn: InputStream, stdOut: OutputStream, stdErr: OutputStream): Int {
-    val process = ProcessBuilder(mutableListOf("ls") + args)
-        .directory(context.getCurrentWorkingDirectory().toFile())
-        .start()
-
-    Executors.newVirtualThreadPerTaskExecutor().use { executor ->
-      val stdErrTransfer = executor.submit { process.errorStream.transferTo(stdErr) }
-      val stdOutTransfer = executor.submit { process.inputStream.transferTo(stdOut) }
-      stdOutTransfer.get()
-      stdErrTransfer.get()
-      process.waitFor()
-      return process.exitValue()
+class LsCommand(private val context: Context, private val args: List<String>) : TextCapableCommand {
+  override fun execute(stdIn: Input, stdOut: Output, stdErr: Output): Int {
+    val path = if (args.isEmpty()) {
+      context.getCurrentWorkingDirectory()
+    } else {
+      context.getCurrentWorkingDirectory().resolve(args[0]).normalize()
     }
+
+    if (!path.toFile().isDirectory) {
+      stdErr.writer.println("ls: ${args[0]}: No such directory")
+      return 1
+    }
+
+    val files = path.toFile().listFiles() ?: throw CommandException(1, "ls: ${args[0]}: Unable to list directory")
+    files.sortedBy { it.name }.forEach { stdOut.writer.println(it.name) }
+
+    return 0
   }
 
   @CommandBuilderDescriptor("ls", ["dir"])
