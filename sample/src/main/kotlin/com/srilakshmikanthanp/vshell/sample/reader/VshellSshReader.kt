21fe1a9c07ab15694808b8073b50e9e60460e4cb
srilakshmikanthanp <srilakshmikanthanp@gmail.com>
2026-04-12 11:22:58 +0530

    Handle EndOfFile and UserInterrupt exceptions in VshellSshReader

--- sample/src/main/kotlin/com/srilakshmikanthanp/vshell/sample/reader/VshellSshReader.kt
+++ sample/src/main/kotlin/com/srilakshmikanthanp/vshell/sample/reader/VshellSshReader.kt
@@ -1,11 +1,21 @@
 package com.srilakshmikanthanp.vshell.sample.reader
 
 import com.srilakshmikanthanp.vshell.jvm.context.Context
+import com.srilakshmikanthanp.vshell.jvm.shell.VshellEndOfFileException
+import com.srilakshmikanthanp.vshell.jvm.shell.VshellInterruptException
 import com.srilakshmikanthanp.vshell.jvm.shell.VshellReader
+import org.jline.reader.EndOfFileException
 import org.jline.reader.LineReader
+import org.jline.reader.UserInterruptException
 
 class VshellSshReader(private val username: String, private val hostname: String, private val reader: LineReader): VshellReader {
   override fun read(context: Context): String {
-    return reader.readLine("$$username@$hostname:${context.getCurrentWorkingDirectory()}> ")
+    try {
+      return reader.readLine("$$username@$hostname:${context.getCurrentWorkingDirectory()}> ")
+    } catch (e: EndOfFileException) {
+      throw VshellEndOfFileException(e.partialLine, "EOF", e)
+    } catch (e: UserInterruptException) {
+      throw VshellInterruptException(e.partialLine, "Interrupted", e)
+    }
   }
 }
